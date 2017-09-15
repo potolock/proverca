63f45f597f4e115ba4ae76a5836d340f3dbacd7d
potolock <potolock@bk.ru>
2017-09-15 17:26:16 +0300

    в файл класса добавлен метод для авторизации

--- group.py
+++ group.py
@@ -3,3 +3,8 @@ class Group:
     def __init__(self, name, footer):
         self.name = name
         self.footer = footer
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        
