8a29e3d42107018da5010db25fc02b31179ef22f
potolock <potolock@bk.ru>
2017-09-15 18:49:04 +0300

    кпе

--- group.py
+++ group.py
@@ -2,4 +2,5 @@
 class Group:
     def __init__(self):
         self.name = name
+        self.header = header
         self.footer = footer
--- proverca_test.py
+++ proverca_test.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
 
 def is_alert_present(wd):
     try:
@@ -12,17 +11,17 @@ def is_alert_present(wd):
 
 class proverca_test(unittest.TestCase):
     def setUp(self):
-        sself.wd = WebDriver(capabilities={"marionette": False})
+        self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
     def test_proverca_test(self):
         success = True
         wd = self.wd
         self.open(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
         self.init_group_creation(wd)
-        self.fill_group_form(wd)
+        self.fill_group_form(wd, "name", "header", "group")
         self.submit_group_create(wd)
         self.return_to_groups_page(wd)
         self.logout(wd)
@@ -39,17 +38,17 @@ class proverca_test(unittest.TestCase):
         # submit group create
         wd.find_element_by_name("submit").click()
 
-    def fill_group_form(self, wd):
+    def fill_group_form(self, wd, name, header, group):
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("name")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("header")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("group")
+        wd.find_element_by_name("group_footer").send_keys(group)
 
     def init_group_creation(self, wd):
         # init group creation
@@ -59,14 +58,14 @@ class proverca_test(unittest.TestCase):
         # open groups page
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         # login
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open(self, wd):
