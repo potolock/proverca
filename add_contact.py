15de00bab883c38904d0f3cc852f9073da81997f
potolock <potolock@bk.ru>
2017-09-18 13:05:21 +0300

    записан рекордером тест

---
+++ add_contact.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time, unittest
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class add_contact(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver(capabilities={"marionette": False})
+        self.wd.implicitly_wait(60)
+    
+    def test_add_contact(self):
+        success = True
+        wd = self.wd
+        # open home page
+        wd.get("http://localhost:8080/addressbook/")
+        # login
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        # open contact page
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("krasnova")
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("natalia")
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("moscow")
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("potolock@bk.ru")
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys("www.site.ru")
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys("frkes")
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys("jdfhgkresgkri")
+        # submit contact
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+    
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
