e2afc5765396486f93b2ada7d9786a44475be658
potolock <potolock@bk.ru>
2017-09-18 14:17:30 +0300

    тест успешно пройден

--- add_contact.py
+++ add_contact.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import  unittest
+from group_contact import Group
+
 
 def is_alert_present(wd):
     try:
@@ -10,31 +11,22 @@ def is_alert_present(wd):
     except:
         return False
 
+
 class add_contact(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
-    def add_contact(self):
+    def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_contact_page(wd)
-        self.fill_contacts_form(wd, firstname="krasnova", lastname="natalia", address="moscow", email="potolock@bk.ru", homepage="www.site.ru", address2="frkes",
-                                notes="jdfhgkresgkri")
+        self.fill_contacts_form(wd, Group(firstname="krasnova", lastname="natalia", address="moscow", email="potolock@bk.ru", homepage="www.site.ru", address2="frkes", notes="jdfhgkresgkri"))
         self.submit_contact(wd)
         self.logout(wd)
 
-        def add_empty_contact(self):
-            wd = self.wd
-            self.open_home_page(wd)
-            self.login(wd, username="admin", password="secret")
-            self.open_contact_page(wd)
-            self.fill_contacts_form(wd, firstname="", lastname="", address="",
-                                    email="", homepage="", address2="",
-                                    notes="")
-            self.submit_contact(wd)
-            self.logout(wd)
+
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
@@ -42,28 +34,28 @@ class add_contact(unittest.TestCase):
     def submit_contact(self, wd):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def fill_contacts_form(self, wd, firstname, lastname, address, email, homepage, address2, notes):
+    def fill_contacts_form(self, wd, group):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(group.address)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email").send_keys(group.email)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(homepage)
+        wd.find_element_by_name("homepage").send_keys(group.homepage)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(address2)
+        wd.find_element_by_name("address2").send_keys(group.address2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(notes)
+        wd.find_element_by_name("notes").send_keys(group.notes)
 
     def open_contact_page(self, wd):
         wd.find_element_by_link_text("add new").click()
