b3a611d7bbda17133616d66a4e78d220316a83eb
potolock <potolock@bk.ru>
2017-09-18 14:17:07 +0300

    создан отдельный класс

---
+++ group_contact.py
@@ -0,0 +1,10 @@
+
+class Group:
+    def __init__(self, firstname, lastname, address, email, homepage, address2, notes):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.email = email
+        self.homepage = homepage
+        self.address2 = address2
+        self.notes = notes
