062d64e772ed7de5c890134a980f97ccdf38bd2d
potolock <potolock@bk.ru>
2017-09-19 12:30:08 +0300

    доработка названия ф-ла класса

---
+++ group_contacty.py
@@ -0,0 +1,9 @@
+
+class Contact:
+    def __init__(self, firstname, lastname, company, address, email, homepage):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.company = company
+        self.address = address
+        self.email = email
+        self.homepage = homepage
