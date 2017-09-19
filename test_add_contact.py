26c3b90f4cc2584144fea4702faf300f12e962e1
potolock <potolock@bk.ru>
2017-09-19 14:36:57 +0300

    тест

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,85 +1,26 @@
-
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import  unittest
+import pytest
 from group_contacty import Contact
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    
-    def test_test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.add_new_contact(wd, Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru"))
-        self.return_home_page(wd)
-        self.logout(wd)
 
-    def test_test_add_empty_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.add_new_contact(wd, Contact(firstname="", lastname="", company="", address="", email="", homepage=""))
-        self.return_home_page(wd)
-        self.logout(wd)
 
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
+def test_test_add_contact(app):
+    app.login( username="admin", password="secret")
+    app.fill_new_contact( Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru"))
+    app.submit()
+    app.logout()
 
-    def return_home_page(self, wd):
-        # returb home page
-        wd.find_element_by_link_text("home page").click()
-
-    def add_new_contact(self, wd, group):
-        # add new contact
-        wd.find_element_by_link_text("add new").click()
-        # fill form page
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group.lastname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(group.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(group.address)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(group.email)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(group.homepage)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def login(self, wd, username="admin", password="secret"):
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self, wd):
-        # open page
-        wd.get("http://localhost:8080/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
-
+def test_test_add_empty_contact(app):
+     app.login( username="admin", password="secret")
+     app.fill_new_contact( Contact(firstname="", lastname="", company="", address="", email="", homepage=""))
+     app.submit()
+     app.logout()
 
