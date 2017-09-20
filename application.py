03718919f399ec39326cbe9f8f4563df043f0823
potolock <potolock@bk.ru>
2017-09-20 18:05:29 +0300

    выделена фикстура

--- application.py
+++ application.py
@@ -2,66 +2,59 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 
 class Application:
+    self.wd = WebDriver(capabilities={"marionette": False})
+    self.wd.implicitly_wait(60)
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
-
-    def logout(self):
+  def logout(self):
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def return_contact_page(self):
+    def return_to_group_page(self):
         wd = self.wd
-        wd.find_element_by_link_text("home page").click()
+        wd.find_element_by_link_text("group page").click()
 
-    def submit(self):
+    def create_group(self, group):
         wd = self.wd
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.return_contact_page()
+        self.open_group_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_group_page()
 
-    def fill_new_contact(self, group):
+    def open_group_page(self):
         wd = self.wd
-        self.open_contact_page()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group.lastname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(group.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(group.address)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(group.email)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(group.homepage)
+        # open group page
+        wd.find_element_by_link_text("groups").click()
 
-    def open_contact_page(self):
+    def login(self, user="admin", password="secret"):
         wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, username="admin", password="secret"):
-        wd = self.wd
-        self.open_page()
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("user").send_keys(user)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def open_page(self):
+    def open_home_page(self):
         wd = self.wd
-        # open page
         wd.get("http://localhost:8080/addressbook/")
 
     def destroy (self):
         self.wd.quit()
 
+
+if __name__ == '__main__':
+    unittest.main()
