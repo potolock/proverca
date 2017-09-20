c2040c7cefb080866e1b414cc3922d1b3da31c56
potolock <potolock@bk.ru>
2017-09-20 16:50:45 +0300

    фигня

--- group_cont.py
+++
@@ -1,9 +0,0 @@
-
-class Contact:
-    def __init__(self, firstname, lastname, company, address, email, homepage):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.company = company
-        self.address = address
-        self.email = email
-        self.homepage = homepage
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group_contacty import Contact
-from application import Application
+
+from fixture.application import Application
+from model.group import Group
 
 
 @pytest.fixture
@@ -11,16 +12,13 @@ def app(request):
     return fixture
 
 
-
-def test_test_add_contact(app):
-    app.login( username="admin", password="secret")
-    app.fill_new_contact( Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru"))
-    app.submit()
+def test_test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create(Group(name="jehfeorwijoe", header="ryg3ky4urgfk34utg", footer="3ye2qy3urk3quwyegf"))
     app.logout()
 
-def test_test_add_empty_contact(app):
-     app.login( username="admin", password="secret")
-     app.fill_new_contact( Contact(firstname="", lastname="", company="", address="", email="", homepage=""))
-     app.submit()
-     app.logout()
+def test_test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create(Group(name="", header="", footer=""))
+    app.logout()
 
