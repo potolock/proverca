00e61f75529f5450a118a8dd78ac6a7c65c98f7a
potolock <potolock@bk.ru>
2017-09-21 17:35:29 +0300

    шибка

--- fixture/group.py
+++ fixture/group.py
@@ -34,4 +34,49 @@ class GroupHelper_group:
          wd.find_element_by_link_text("groups").click()
 
 
+class GroupHelper_contact:
+    def __init__(self, app):
+        self.app = app
+
+    def return_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def submit(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_contact_page()
+
+    def fill_new_contact(self, group):
+        wd = self.app.wd
+        self.open_contact_page()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(group.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(group.address)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(group.email)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(group.homepage)
+
+    def open_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def open_page(self):
+        wd = self.app.wd
+        # open page
+        wd.get("http://localhost:8080/addressbook/")
+
+
 
