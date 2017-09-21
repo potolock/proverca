d56f42fb18200c67a53df120fe51db58e63eae6c
potolock <potolock@bk.ru>
2017-09-21 16:11:52 +0300

    выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,50 +1,20 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper_group
+from fixture.group import GroupHelper_group
 
 
-class Application_grup:
+class Application_group:
 
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
         self.session = SessionHelper_group(self)
-
-
-
-    def return_to_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_group_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
-
-    def open_group_page(self):
-         wd = self.wd
-         # open group page
-         wd.find_element_by_link_text("groups").click()
-
-
+        self.group = GroupHelper_group (self)
 
     def open_home_page(self):
-         wd = self.wd
-         wd.get("http://localhost:8080/addressbook/")
-
+        wd = self.wd
+        wd.get("http://localhost:8080/addressbook/")
 
     def destroy (self):
         self.wd.quit()
+
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,25 +1,25 @@
 
 import pytest
 
-from fixture.application import Application_grup
+from fixture.application import Application_group
 from model.group import Group
 
 
 @pytest.fixture
 def app(request):
-    fixture = Application_grup()
+    fixture = Application_group()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_test_add_group(app):
      app.session.login(user="admin", password="secret")
-     app.create_group( Group(name="jehfeorwijoe", header="ryg3ky4urgfk34utg", footer="3ye2qy3urk3quwyegf"))
+     app.group.create(Group(name="jehfeorwijoe", header="ryg3ky4urgfk34utg", footer="3ye2qy3urk3quwyegf"))
      app.session.logout()
 
 def test_test_add_empty_group(app):
     app.session.login(user="admin", password="secret")
-    app.create_group( Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
 
