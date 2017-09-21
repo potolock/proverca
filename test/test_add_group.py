c9d8dc09eb90c2a7bff47e25dd04de3c1dec10fd
potolock <potolock@bk.ru>
2017-09-21 14:14:52 +0300

    выделен класс для управления сессиями

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,26 +1,26 @@
 
 import pytest
 
-from fixture.application_group import Application
+from fixture.application import Application_grup
 from model.group import Group
 
 
 @pytest.fixture
 def app(request):
-    fixture = Application()
+    fixture = Application_grup()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_test_add_group(app):
-     app.login(user="admin", password="secret")
+     app.session.login(user="admin", password="secret")
      app.create_group( Group(name="jehfeorwijoe", header="ryg3ky4urgfk34utg", footer="3ye2qy3urk3quwyegf"))
-     app.logout()
+     app.session.logout()
 
 def test_test_add_empty_group(app):
-    app.login(user="admin", password="secret")
+    app.session.login(user="admin", password="secret")
     app.create_group( Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
 
