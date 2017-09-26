9fdbb64e04ee0908002807edebc9435a699b664a
potolock <potolock@bk.ru>
2017-09-26 15:51:25 +0300

    добавлен тест

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -7,10 +7,9 @@ def test_test_add_contact(app):
     app.session.login( username="admin", password="secret")
     app.contact.fill_new(Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru"))
     app.contact.submit()
-    # app1.session.logout()
+
 
 def test_add_empty_contact(app):
-     # app1.session.login( username="admin", password="secret")
      app.contact.fill_new(Contact(firstname="", lastname="", company="", address="", email="", homepage=""))
      app.contact.submit()
      app.session.logout()
