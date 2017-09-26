e4245d72c73a800a22f6db59fd23117d92316fc8
potolock <potolock@bk.ru>
2017-09-26 16:22:13 +0300

    помощники

--- fixture/session.py
+++ fixture/session.py
@@ -1,8 +1,10 @@
 
 class  SessionHelper:
 
+
     def __init__(self, app):
         self.app = app
+        
 
     def login(self, username="admin", password="secret"):
         wd = self.app.wd
@@ -15,9 +17,11 @@ class  SessionHelper:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
+
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
 
 
 
