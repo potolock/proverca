059f48200a9e08afcaf5859017b2810fe6f53ce9
potolock <potolock@bk.ru>
2017-09-26 16:41:29 +0300

    лрл

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,7 @@ from model.group import Group
 def test_add_group(app):
      app.session.login(username="admin", password="secret")
      app.group.create(Group(name="jehfeorwijoe", header="ryg3ky4urgfk34utg", footer="3ye2qy3urk3quwyegf"))
-     # app.session.logout()
+
 
 def test_add_empty_group(app):
     # app.session.login(user="admin", password="secret")
