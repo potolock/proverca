23fae351fd27107b7457dda5bba04d4a1694a1ee
potolock <potolock@bk.ru>
2017-09-26 18:50:07 +0300

    проверка валидности фикстуры для групп

--- fixture/application.py
+++ fixture/application.py
@@ -14,6 +14,12 @@ class Application:
         self.contact = GroupHelper_contact(self)
         self.group = GroupHelper_group (self)
 
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
 
     def open_home_page(self):
         wd = self.wd
