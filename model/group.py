83079038639912a3d7cedf1fc065c0030e70be5d
potolock <potolock@bk.ru>
2017-09-27 19:04:26 +0300

    добавлены дефолт. значения для контактов

--- model/group.py
+++ model/group.py
@@ -8,7 +8,7 @@ class Group:
 
 
 class Contact:
-    def __init__(self, firstname, lastname, company, address, email, homepage):
+    def __init__(self, firstname=None, lastname=None, company=None, address=None, email=None, homepage=None):
         self.firstname = firstname
         self.lastname = lastname
         self.company = company
