2bbf3e6ad24e8c892d2ee83c87a754f01555ebd6
potolock <potolock@bk.ru>
2017-09-27 19:03:41 +0300

    создан новый вспомог. метод для контактов

--- fixture/group.py
+++ fixture/group.py
@@ -1,6 +1,6 @@
 
 
-class GroupHelper_group:
+class Helper_group:
       def __init__(self, app):
         self.app = app
 
@@ -64,7 +64,7 @@ class GroupHelper_group:
          wd.find_element_by_link_text("groups").click()
 
 
-class GroupHelper_contact:
+class Helper_contact:
     def __init__(self, app):
         self.app = app
 
@@ -72,6 +72,20 @@ class GroupHelper_contact:
     def return_contact_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
+
+    def modify_first_contact(self, new_contact_data):
+        wd = self.app.wd
+        # self.open_contact_page()
+        # self.select_first_contact()
+        # open modification form
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        # fill group form
+        self.fill_contact_form(new_contact_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
+        self.return_contact_page()
+
+
 
     def submit(self):
         wd = self.app.wd
@@ -82,36 +96,44 @@ class GroupHelper_contact:
 
     def delete_first_contact(self):
         wd = self.app.wd
-        # select first contact
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_contact()
         # submit deletion
         wd.find_element_by_xpath("//*[@value='Delete']").click()
         # selecr action
         wd.switch_to_alert().accept()
 
+    def select_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def fill_new(self, group):
         wd = self.app.wd
         self.open_contact_page()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group.lastname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(group.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(group.address)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(group.email)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(group.homepage)
+        # init contact create
+        wd.find_element_by_link_text("add new").click()
+        self.fill_contact_form(group)
+        # submit contact creation
+        wd.find_element_by_name("submit").click()
+        self.return_contact_page()
 
+
+    def fill_contact_form(self, contact):
+        wd = self.app.wd
+        self.change_field_contact_value("firstname", contact.firstname)
+        self.change_field_contact_value("lastname", contact.lastname)
+        self.change_field_contact_value("company", contact.company)
+        self.change_field_contact_value("address", contact.address)
+        self.change_field_contact_value("email", contact.email)
+        self.change_field_contact_value("homepage", contact.homepage)
+
+
+
+    def change_field_contact_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def open_contact_page(self):
         wd = self.app.wd
