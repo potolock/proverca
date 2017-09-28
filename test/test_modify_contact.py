5658c642118e6030790f4ceaf158e3c0ce26b662
potolock <potolock@bk.ru>
2017-09-28 14:11:55 +0300

    в тест модиф. контакта добавлена проверка

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,8 +4,12 @@
 from model.group import Contact
 
 def test_test_modify_contact_firstname(app):
+    if app.contact.count() ==0:
+        app.contact.fill_new(Contact(lastname="tester"))
     app.contact.modify_first_contact(Contact(firstname="Veronica"))
 
 
 def test_test_modify_contact_address(app):
+    if app.contact.count() ==0:
+        app.contact.fill_new(Contact(company="tester"))
     app.contact.modify_first_contact(Contact(address="Moscow"))
