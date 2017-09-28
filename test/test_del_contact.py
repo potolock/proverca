c9653f513e15e3d41a8cc7889d3b3457501f1bb6
potolock <potolock@bk.ru>
2017-09-28 14:03:53 +0300

    в тест удаления контакта добавлена проверка на наличие

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,7 +1,10 @@
 
+from model.group import Contact
 
 
 def test_delete_contact(app):
+    if app.contact.count() == 0:
+        app.contact.fill_new(Contact(firstname="tester"))
     app.contact.delete_first_contact()
 
 
