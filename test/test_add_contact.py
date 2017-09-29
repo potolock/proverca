7be60d01718556e71d1652413d5d327e4bfdacca
potolock <potolock@bk.ru>
2017-09-29 17:40:26 +0300

    исправлено условие проверки для страницы контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,11 +5,11 @@ from model.group import Contact
 
 def test_test_add_contact(app):
     app.contact.fill_new(Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru"))
-    # app.contact.submit()
 
 
-#def test_add_empty_contact(app):
-     #app.contact.fill_new(Contact(firstname="", lastname="", company="", address="", email="", homepage=""))
-     #app.contact.submit()
+
+def test_add_empty_contact(app):
+     app.contact.fill_new(Contact(firstname="", lastname="", company="", address="", email="", homepage=""))
+
 
 
