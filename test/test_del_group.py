2936b513b54f2769ab3cfa4643bc00dfdcff337c
potolock <potolock@bk.ru>
2017-09-29 17:45:55 +0300

    исправлена опечатка в тесте удаления группы

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -5,6 +5,6 @@ from model.group import Group
 
 def test_test_delete_first_group(app):
     app.group.delete_first_group()
-    if app.group.cont() == 0:
+    if app.group.count() == 0:
         app.group.create (Group(name="test"))
     app.group.delete_first_group()
