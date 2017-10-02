520c447d75423e424ca398888990f7673566de0f
potolock <potolock@bk.ru>
2017-10-02 17:45:05 +0300

    добавлен метод в group для контактов

--- fixture/group.py
+++ fixture/group.py
@@ -1,21 +1,35 @@
 
 
+from model.group import Group
+
+
 class Helper_group:
-      def __init__(self, app):
-        self.app = app
+     def __init__(self, app):
+          self.app = app
+
+     def get_group_list(self):
+         wd = self.app.wd
+         self.open_group_page()
+         groups = []
+         for element in wd.find_elements_by_css_selector("span.group"):
+             text = element.text
+             id = element.find_elements_by_name("selected[]").get_attribute("value")
+             groups.append(Group(name=text, id=id))
+         return groups
 
 
-      def count(self):
+
+     def count(self):
         wd = self.app.wd
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
 
 
-      def return_to_group_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
+     def return_to_group_page(self):
+         wd = self.app.wd
+         wd.find_element_by_link_text("group page").click()
 
-      def modify_first_group(self, new_group_data):
+     def modify_first_group(self, new_group_data):
           wd = self.app.wd
           self.open_group_page()
           self.select_first_group()
@@ -27,7 +41,7 @@ class Helper_group:
           wd.find_element_by_name("update").click()
           self.return_to_group_page()
 
-      def delete_first_group (self):
+     def delete_first_group (self):
           wd = self.app.wd
           self.open_group_page()
           self.select_first_group()
@@ -36,36 +50,36 @@ class Helper_group:
           self.return_to_group_page()
 
 
-      def select_first_group(self):
+     def select_first_group(self):
           wd = self.app.wd
           wd.find_element_by_name("selected[]").click()
 
 
-      def create(self, group):
-        wd = self.app.wd
-        self.open_group_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        self.fill_group_form(group)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
+     def create(self, group):
+         wd = self.app.wd
+         self.open_group_page()
+         # init group creation
+         wd.find_element_by_name("new").click()
+         self.fill_group_form(group)
+         # submit group creation
+         wd.find_element_by_name("submit").click()
+         self.return_to_group_page()
 
-      def fill_group_form(self, group):
-          wd = self.app.wd
-          self.change_field_value("group_name", group.name)
-          self.change_field_value("group_header", group.header)
-          self.change_field_value("group_footer", group.footer)
+     def fill_group_form(self, group):
+         wd = self.app.wd
+         self.change_field_value("group_name", group.name)
+         self.change_field_value("group_header", group.header)
+         self.change_field_value("group_footer", group.footer)
 
 
-      def change_field_value(self, field_name, text):
-          wd = self.app.wd
-          if text is not None:
-              wd.find_element_by_name(field_name).click()
-              wd.find_element_by_name(field_name).clear()
-              wd.find_element_by_name(field_name).send_keys(text)
+     def change_field_value(self, field_name, text):
+         wd = self.app.wd
+         if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
-      def open_group_page(self):
+     def open_group_page(self):
          wd = self.app.wd
          if not (wd.current_url.endswith("group.php") and len (wd.find_elements_by_name("new")) > 0):
             wd.find_element_by_link_text("groups").click()
