137ad0894abcabdbab268263c7efe3b02959f9e6
potolock <potolock@bk.ru>
2017-10-02 19:03:18 +0300

    редактирование тесты  модифицирования групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,13 +4,15 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    if app.group.count() == 0:
-       app.group.create (Group(name="tester"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="Neww group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 
 def test_modify_group_footer(app):
-    if app.group.count() == 0:
-       app.group.create (Group(footer="tester"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(footer="Neww footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
