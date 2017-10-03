882bd623a7dbf05b892b4126acdf50bdf966aac0
potolock <potolock@bk.ru>
2017-10-03 15:59:09 +0300

    добавлены новые функции для сравнения

--- model/group.py
+++ model/group.py
@@ -1,4 +1,6 @@
 
+
+
 class Group:
 
     def __init__(self, name=None, header=None, footer=None, id=None):
@@ -6,6 +8,12 @@ class Group:
         self.header = header
         self.footer = footer
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name ==other.name
 
 class Contact:
 
