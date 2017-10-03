5c362106b3e20c3305619b84e9c13208dc089815
potolock <potolock@bk.ru>
2017-10-03 16:12:48 +0300

    добавлены функции для сравнения контакта

--- model/group.py
+++ model/group.py
@@ -13,14 +13,22 @@ class Group:
         return "%s:%s" % (self.id, self.name)
 
     def __eq__(self, other):
-        return self.id == other.id and self.name ==other.name
+        return self.id == other.id and self.name == other.name
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, company=None, address=None, email=None, homepage=None):
+    def __init__(self, firstname=None, lastname=None, company=None, address=None, email=None, homepage=None, id=None):
         self.firstname = firstname
         self.lastname = lastname
         self.company = company
         self.address = address
-        self.email = email
         self.homepage = homepage
+        self.email = email
+        self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.email)
+
+    def __eq__(self, other):
+        return  self.id ==other.id and self.email == other.email
+
