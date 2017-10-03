18d862f5c0d202300f290ca0402aa12339d17406
potolock <potolock@bk.ru>
2017-10-03 16:52:56 +0300

    добавлены проверки в тест добавления контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,17 +5,21 @@ from model.group import Contact
 
 def test_test_add_contact(app):
     old_contact = app.contact.get_contact_list()
-    app.contact.fill_new(Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru"))
+    contact = Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru")
+    app.contact.fill_new(contact)
     new_contact = app.contact.get_contact_list()
     assert len(old_contact) + 1 == len(new_contact)
-
+    old_contact.append (contact)
+    assert  sorted (old_contact, key=Contact.id_or_max) == sorted (new_contact, key=Contact.id_or_max)
 
 
 def test_add_empty_contact(app):
     old_contact = app.contact.get_contact_list()
-    app.contact.fill_new(Contact(firstname="", lastname="", company="", address="", email="", homepage=""))
+    contact = Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru")
+    app.contact.fill_new(contact)
     new_contact = app.contact.get_contact_list()
     assert len(old_contact) + 1 == len(new_contact)
-
+    old_contact.append(contact)
+    assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
 
 
