4c6979469f1a3e7536bc504cf53fdb1b224c3a98
potolock <potolock@bk.ru>
2017-10-03 16:12:09 +0300

    добавлено сравнение списков контактов  в тесте удал.

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -9,5 +9,6 @@ def test_delete_contact(app):
     app.contact.delete_first_contact()
     new_contact = app.contact.get_contact_list()
     assert len(old_contact) - 1 == len(new_contact)
-
+    old_contact[0:1] = []
+    assert old_contact == new_contact
 
