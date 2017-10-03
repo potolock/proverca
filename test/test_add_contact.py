8cf8847c2cb7f5df5c42ab8ec7473e290867e963
potolock <potolock@bk.ru>
2017-10-03 15:21:17 +0300

    добавлены сравнения для списков в тест добавления контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,12 +4,18 @@ from model.group import Contact
 
 
 def test_test_add_contact(app):
+    old_contact = app.contact.get_contact_list()
     app.contact.fill_new(Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru"))
+    new_contact = app.contact.get_contact_list()
+    assert len(old_contact) + 1 == len(new_contact)
 
 
 
 def test_add_empty_contact(app):
-     app.contact.fill_new(Contact(firstname="", lastname="", company="", address="", email="", homepage=""))
+    old_contact = app.contact.get_contact_list()
+    app.contact.fill_new(Contact(firstname="", lastname="", company="", address="", email="", homepage=""))
+    new_contact = app.contact.get_contact_list()
+    assert len(old_contact) + 1 == len(new_contact)
 
 
 
