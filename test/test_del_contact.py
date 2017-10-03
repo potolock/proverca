7b2ce866f70b5ffa752ff6e39b0c58412b33b9e9
potolock <potolock@bk.ru>
2017-10-03 15:35:16 +0300

    добавлена проверка для списка  при удалении контакта

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -5,6 +5,9 @@ from model.group import Contact
 def test_delete_contact(app):
     if app.contact.count() == 0:
         app.contact.fill_new(Contact(firstname="tester"))
+    old_contact = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contact = app.contact.get_contact_list()
+    assert len(old_contact) - 1 == len(new_contact)
 
 
