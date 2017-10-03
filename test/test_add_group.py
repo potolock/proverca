1229d63d636018aceed1599c4dcd408aca7314c8
potolock <potolock@bk.ru>
2017-10-03 16:28:23 +0300

    сравнение списков групп тест добавл. группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,9 +5,12 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="jehfeorwijoe", header="ryg3ky4urgfk34utg", footer="3ye2qy3urk3quwyegf"))
+    group = Group(name="jehfeorwijoe", header="ryg3ky4urgfk34utg", footer="3ye2qy3urk3quwyegf")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len (old_groups) + 1 == len (new_groups)
+    old_groups.append(group)
+    assert sorted (old_groups, key=Group.id_or_max ) == sorted (new_groups, key=Group.id_or_max)
 
 def test_add_empty_group(app):
      old_groups = app.group.get_group_list()
