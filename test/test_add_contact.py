067354badc2f6d9f35d3ef0fc74ef74f8e5543d0
potolock <potolock@bk.ru>
2017-10-04 17:41:24 +0300

    исправлен тест добавления контакта для сравнения списков

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from sys import maxsize
 
 from model.group import Contact
 
 
-def test_test_add_contact(app):
+def test_add_contact(app):
     old_contact_list = app.contact.get_contact_list()
-    contact = Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru")
+    contact = Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf",
+                      email="", homepage="site.ru")
     app.contact.fill_new(contact)
     new_contact_list = app.contact.get_contact_list()
     assert len(old_contact_list) + 1 == len(new_contact_list)
@@ -14,13 +14,13 @@ def test_test_add_contact(app):
     assert  sorted (old_contact_list, key=Contact.id_or_max) == sorted (new_contact_list, key=Contact.id_or_max)
 
 
-#def test_add_empty_contact(app):
-#    old_contact_list = app.contact.get_contact_list()
- #   contact = Contact(firstname="natalia", lastname="krasnova", company="jhkjh", address="fewderf", email="email@email.ru", homepage="site.ru")
- #   app.contact.fill_new(contact)
-  #  new_contact_list = app.contact.get_contact_list()
-  #  assert len(old_contact_list) + 1 == len(new_contact_list)
- #   old_contact_list.append(contact)
- #   assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
+def test_add_empty_contact(app):
+   old_contact_list = app.contact.get_contact_list()
+   contact = Contact(firstname="", lastname="", company="", address="", email="", homepage="")
+   app.contact.fill_new(contact)
+   new_contact_list = app.contact.get_contact_list()
+   assert len(old_contact_list) + 1 == len(new_contact_list)
+   old_contact_list.append(contact)
+   assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
 
 
