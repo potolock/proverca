27f50924146ab85d3037b5bad3c0c4c13cdf862b
potolock <potolock@bk.ru>
2017-10-06 14:48:46 +0300

    тест удал. контакта - кэш + проверка длины

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -5,10 +5,10 @@ from model.group import Contact
 def test_delete_contact(app):
     if app.contact.count() == 0:
         app.contact.fill_new(Contact(firstname="tester"))
-    old_contact = app.contact.get_contact_list()
+    old_contact_list = app.contact.get_contact_list()
     app.contact.delete_first_contact()
-    assert len(old_contact) - 1 == app.contact.count()
-    new_contact = app.contact.get_contact_list()
-    old_contact[0:1] = []
-    assert old_contact == new_contact
+    assert len(old_contact_list) - 1 == app.contact.count()
+    new_contact_list = app.contact.get_contact_list()
+    old_contact_list[0:1] = []
+    assert old_contact_list == new_contact_list
 
