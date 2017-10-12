dd118f607758f5fa1d016852fdf1c2d240a65a42
potolock <potolock@bk.ru>
2017-10-12 18:30:55 +0300

    создан тест для проверки случ. выбр. контакта+помощники

--- model/group.py
+++ model/group.py
@@ -26,22 +26,23 @@ class Group:
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, id=None, all_phones_from_home_page=None, homephone=None, mobilephone=None, workphone=None, secondaryphone=None, address=None, company=None, email=None, homepage=None):
-        self.firstname = firstname
+    def __init__(self, firstname=None, lastname=None, id=None, all_emails_from_home_page=None, all_phones_from_home_page=None, homephone=None, mobilephone=None, workphone=None, secondaryphone=None, address=None, email=None, email2=None, email3=None):
         self.lastname = lastname
+        self.firstname = firstname
         self.address = address
-        self.company = company
-        self.email = email
-        self.homepage = homepage
         self.homephone = homephone
         self.mobilephone = mobilephone
         self.workphone = workphone
         self.secondaryphone = secondaryphone
         self.id = id
+        self.email = email
+        self.email2 = email2
+        self.email3= email3
+        self.all_emails_from_home_page = all_emails_from_home_page
         self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s:%s:%s:%s" % (self.id, self.firstname, self.lastname, self.homephone, self.all_phones_from_home_page, self.mobilephone, self.workphone, self.secondaryphone, self.address)
+        return "%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s" % (self.id, self.firstname, self.lastname, self.homephone, self.all_phones_from_home_page, self.mobilephone, self.workphone, self.secondaryphone, self.address, self.email, self.email2, self.email3, self.all_emails_from_home_page)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname
