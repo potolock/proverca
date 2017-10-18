44f7af30a2f92c020c1f1e44bf20aa8a6cb6bb41
potolock <potolock@bk.ru>
2017-10-18 17:56:19 +0300

    использование библиотеки jsonpickle  для преобраз. в json

--- generator/contact.py
+++ generator/contact.py
@@ -4,9 +4,11 @@ from model.group import Contact
 import random
 import string
 import os.path
-import json
 import getopt
 import sys
+import jsonpickle
+
+
 
 
 
@@ -43,5 +45,6 @@ testdata = [ Contact(firstname="", lastname="", company="", address="", email=""
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as f:
-    f.write(json.dumps(testdata, default = lambda x: x.__dict__, indent=2))
+with open(file, "w") as out:
+    out.write(jsonpickle.pickler.encode(testdata, default = lambda x: x.__dict__, indent=2))
+
