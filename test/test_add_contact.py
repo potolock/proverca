8403e6a734348a74b3140999b2a2558fecca8a36
potolock <potolock@bk.ru>
2017-10-18 14:30:34 +0300

    загрузка тестовых данных для контактов из файла

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,23 +2,9 @@
 
 from model.group import Contact
 import pytest
-import random
-import string
+from data.add_contact import constant as testdata
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + ""*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-
-
-testdata = [ Contact(firstname="", lastname="", company="", address="", email="", homepage="")] + [
-            Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 15),
-                    company=random_string("company", 10), address=random_string("address", 20),
-                      email=random_string("email", 10), homepage=random_string("homepage", 10))
-    for i in range(5)
-]
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
