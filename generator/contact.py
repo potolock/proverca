45e635418372a68024b7b22de16f5ede5946ad5b
potolock <potolock@bk.ru>
2017-10-19 13:31:38 +0300

    для контактов

--- generator/contact.py
+++ generator/contact.py
@@ -46,5 +46,6 @@ testdata = [ Contact(firstname="", lastname="", company="", address="", email=""
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(jsonpickle.encode(testdata, default = lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
 
