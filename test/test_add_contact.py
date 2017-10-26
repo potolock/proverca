20e0bee04267ea8d35250cd9aaf1d3f1044a1d4a
potolock <potolock@bk.ru>
2017-10-26 13:56:30 +0300

    переделка теста доб. контакта , загрузка из БД

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,12 +2,12 @@
 
 from model.group import Contact
 
-def test_add_contact(app, json_contact):
+def test_add_contact(app, db, json_contact):
         contact = json_contact
-        old_contact_list = app.contact.get_contact_list()
+        old_contact_list = db.get_contact_list()
         app.contact.fill_new(contact)
         assert len(old_contact_list) + 1 == app.contact.count()
-        new_contact_list = app.contact.get_contact_list()
+        new_contact_list = db.get_contact_list()
         old_contact_list.append(contact)
         assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact_list, key=Contact.id_or_max)
 
