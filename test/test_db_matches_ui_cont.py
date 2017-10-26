469efe510dcd75cdc0524bebaa57b7df11f3a3c1
potolock <potolock@bk.ru>
2017-10-26 18:09:41 +0300

    сравнение контактов с главной страницы с инф. БД

--- test/test_db_matches_ui_cont.py
+++ test/test_db_matches_ui_cont.py
@@ -7,5 +7,8 @@ from timeit import timeit
 
 def test_contact_list(app, db):
     ui_list = app.contact.get_contact_list()
-    db_list = db.get_contact_list()
+    def clean(contact):
+        return Contact(id=contact.id, firstname=contact.firstname, lastname=contact.lastname, company=contact.company,
+                       homepage=contact.homepage, address=contact.address.strip())
+    db_list = map(clean, db.get_contact_list())
     assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
