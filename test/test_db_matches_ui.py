d0989ff19088ff8facf2b1d9503060470e752a71
potolock <potolock@bk.ru>
2017-10-26 18:34:05 +0300

    сравнение гр из интерф и бд

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -3,8 +3,6 @@ from model.group import Group
 from timeit import timeit
 
 
-
-
 def test_group_list(app, db):
     print(timeit(lambda: app.group.get_group_list(), number=1))
     def clean(group):
